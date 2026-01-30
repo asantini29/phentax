small bugfix in polarization angle rotation

src/phentax/waveform.py
@@ -968,9 +968,9 @@ class IMRPhenomTHM:
         h_cross_rotated : Array
             Rotated cross polarization strain.
         """
-        cos_2psi = jnp.cos(2.0 * psi)[:, None]
-        sin_2psi = jnp.sin(2.0 * psi)[:, None]
-
+        cos_2psi = jnp.atleast_1d(jnp.cos(2.0 * jnp.array(psi)))[:, None]
+        sin_2psi = jnp.atleast_1d(jnp.sin(2.0 * jnp.array(psi)))[:, None]
+        
         h_plus_rotated = h_plus * cos_2psi - h_cross * sin_2psi
         h_cross_rotated = h_plus * sin_2psi + h_cross * cos_2psi
 